8482fef978332651475395ba6f6c1fdf2f96f561
Bryce <bryceilcan29@gmail.com>
2025-10-16 19:23:58 -0600

    merge conflict

--- app/src/main.c
+++ app/src/main.c
@@ -8,80 +8,15 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
-<<<<<<< HEAD
-#define LED0_NODE DT_ALIAS(led0)
-#define LED1_NODE DT_ALIAS(led1)
-#define LED2_NODE DT_ALIAS(led2)
-#define LED3_NODE DT_ALIAS(led3)
+#define SW0_NODE DT_ALIAS(sw0)
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 
-static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
-static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
-
-int main(void){
-    int ret, ret1, ret2, ret3;
-
-    if(!gpio_is_ready_dt(&led0)){
-        return -1;
-    }
-
-    if(!gpio_is_ready_dt(&led1)){
-        return -1;
-    }
-    if(!gpio_is_ready_dt(&led2)){
-        return -1;
-    }
-    if(!gpio_is_ready_dt(&led3)){
-        return -1;
-    }
-
-    ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
-    ret1 = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
-    ret2 = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
-    ret3 = gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
-    
-    if (ret < 0){
-        return ret;
-    }
-    if (ret1 < 0){
-        return ret1;
-    }
-    if (ret2 < 0){
-        return ret2;
-    }
-    if (ret3 < 0){
-        return ret3;
-    }
-    
-
-    while(1){
-        gpio_pin_toggle_dt(&led0);
-
-        k_msleep(200);
-
-        gpio_pin_toggle_dt(&led1);
-
-        k_msleep(200);
-
-        gpio_pin_toggle_dt(&led2);
-
-        k_msleep(200);
-
-        gpio_pin_toggle_dt(&led3);
-
-        k_msleep(500);
-    }
-
-
-    return 0;
-}
-=======
 int main(void) {
+    int ret;
 
-  while(1) {
 
-  }
-	return 0;
+    while(1) {
+
+    }
+  	return 0;
 }
->>>>>>> 793d0aaf5eedfd38e12b119cc590a06f517ee103
