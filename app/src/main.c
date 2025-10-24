20a72dc203db603c4b1ea3d19fad646c6f960787
Bryce <bryceilcan29@gmail.com>
2025-10-23 18:43:23 -0600

    attempted challenge

--- app/src/main.c
+++ app/src/main.c
@@ -21,57 +21,11 @@ int main(void) {
     }
 
     int seq = 0;
+    int locked = 1;
     while (1) {
 
         
-        switch (seq){
-            case 0:
-                if(BTN_check_clear_pressed(BTN0)){
-                   printk("press 1\n");
-                   seq = 1;
-                }else if (BTN_check_clear_pressed(BTN1) ||
-                        BTN_check_clear_pressed(BTN2) ||
-                        BTN_check_clear_pressed(BTN3)) {
-                            seq = 0;
-                        }
-                break;
-            case 1:
-                if(BTN_check_clear_pressed(BTN1)){
-                    printk("press 2\n");
-                    seq = 2;
-                }else if (BTN_check_clear_pressed(BTN0) ||
-                       BTN_check_clear_pressed(BTN2) ||
-                       BTN_check_clear_pressed(BTN3)) {
-                            seq = 0;
-                       }
-                break;
-            case 2:
-                if(BTN_check_clear_pressed(BTN2)){
-                    printk("press 3\n");
-                    seq = 3;
-                }else if (BTN_check_clear_pressed(BTN0) ||
-                       BTN_check_clear_pressed(BTN1) ||
-                       BTN_check_clear_pressed(BTN3)) {
-                            seq = 0;
-                       }
-                break;
-            case 3:
-                if(BTN_check_clear_pressed(BTN3)){
-                    printk("press 4\n");
-                    LED_set(LED0, LED_ON);
-                    LED_set(LED1, LED_ON);
-                    LED_set(LED2, LED_ON);
-                    LED_set(LED3, LED_ON);
-                    k_msleep(1000);
-                }else if (BTN_check_clear_pressed(BTN0) ||
-                       BTN_check_clear_pressed(BTN1) ||
-                       BTN_check_clear_pressed(BTN2)) {
-                            seq = 0;
-                       }
-                break;
-        }
-        k_msleep(SLEEP_MS);
-                    
+        k_msleep(SLEEP_MS);         
                         
     }
     
